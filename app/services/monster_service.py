b3f97cdbae736db6f05c19a9d2ea05127a1328f4
nickchkonia <macuser@Macs-MacBook-Pro.local>
2025-01-31 21:15:40 +0400

    WIP: add MonsterService

---
+++ app/services/monster_service.py
@@ -0,0 +1,16 @@
+# TODO: import the dungeons_logic package
+
+class MonsterService():
+    @staticmethod
+    def get_monster_by_name(monster_name):
+        pass
+    
+    @staticmethod
+    # TODO: add interface for this
+    def create_monster(monster_data):
+        # TODO: this should be from dungeons_logic
+        new_monster = None
+        
+    @staticmethod
+    def delete_monster_by_id(monster_id):
+        return None
